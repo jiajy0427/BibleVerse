594a61af9453cc9f43d46c56a29a923e3afe56b5
Jialin <jiajy027@gmail.com>
2025-02-14 13:57:46 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using HtmlAgilityPack;
+namespace BibleVerse{
+    class Progam{
+        static void Main(){
+            Console.WriteLine(HtmlEntity.DeEntitize(new HtmlWeb().Load("https://dailyverses.net/random-bible-verse").DocumentNode.QuerySelector("span.v1").QuerySelector(".v1").InnerText));
+        }
+    }
+}
