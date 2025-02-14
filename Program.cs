9f7d77d84bc7c5584492f62c13dea89cbcf30178
Jialin <jiajy027@gmail.com>
2025-02-14 15:55:02 +1100

    added:             Console.WriteLine("");

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 namespace BibleVerse{
     class Progam{
         static void Main(){
+            Console.WriteLine("");
             Console.WriteLine("Your Bible verse of the day: ");
             Console.WriteLine(HtmlEntity.DeEntitize(new HtmlWeb().Load("https://dailyverses.net/random-bible-verse").DocumentNode.QuerySelector("span.v1").QuerySelector(".v1").InnerText));
             Console.WriteLine("");
